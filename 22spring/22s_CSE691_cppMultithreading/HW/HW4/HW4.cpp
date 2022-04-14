bd330d75b01625c6747aa3676b4753db53b8cc85
Yuchen <yuchen@Yuchens-MacBook-Air.local>
2022-04-14 00:36:33 -0400

    ADD: CSE691 HW4 partWorkerWorkflow v1

--- 22spring/22s_CSE691_cppMultithreading/HW/HW4/HW4.cpp
+++ 22spring/22s_CSE691_cppMultithreading/HW/HW4/HW4.cpp
@@ -7,8 +7,10 @@
 #include <atomic>
 
 using namespace std;
+using namespace chrono;
 
-mutex m1, m2;
+mutex m1, m2, buffer_lock;
+condition_variable cv1, cv2;
 
 class Buffer
 {
@@ -48,7 +50,6 @@ class PartWorker
 private:
     int id;
     int size_of_load_order;
-    int wait_time;
     int max_wait_time;
 
 public:
@@ -60,18 +61,6 @@ public:
         return id;
     }
 
-    void resetWaitTime()
-    {
-        wait_time = 0;
-
-        return;
-    }
-
-    bool isWaitOverMax()
-    {
-        return wait_time >= max_wait_time;
-    }
-
     // randomly fill the load till the size of load order
     void reloadLoadOrder();
 
@@ -80,7 +69,6 @@ public:
     PartWorker()
     {
         size_of_load_order = 5;
-        wait_time = 0;
         load_order = vector<int>(size_of_load_order, 0);
         local_state = vector<int>(size_of_load_order, 0);
         reloadLoadOrder();
@@ -102,7 +90,6 @@ class ProductWorker
 private:
     int id;
     int size_of_assembly_order;
-    int wait_time;
     int max_wait_time;
 
 public:
@@ -114,18 +101,6 @@ public:
         return id;
     }
 
-    void resetWaitTime()
-    {
-        wait_time = 0;
-
-        return;
-    }
-
-    bool isWaitOverMax()
-    {
-        return wait_time >= max_wait_time;
-    }
-
     // randomly fill the load till the size of load order
     void reloadAssemblyOrder();
 
@@ -134,7 +109,6 @@ public:
     ProductWorker()
     {
         size_of_assembly_order = 5;
-        wait_time = 0;
         assembly_order = vector<int>(size_of_assembly_order, 0);
         cart_state = vector<int>(size_of_assembly_order, 0);
         reloadAssemblyOrder();
@@ -201,6 +175,32 @@ void testProductWorker(ProductWorker &w2)
     return;
 }
 
+void PartWorkerScheduler(PartWorker &worker, Buffer &buffer, vector<Part> &parts, int num_of_iteration)
+{
+    // each worker to finish 5 iterations
+    // int num_of_iteration = 5;
+
+    while (num_of_iteration-- > 0)
+    {
+        worker.partWorkerWorkflow(buffer, parts);
+    }
+
+    return;
+}
+
+void ProductWorkerScheduler(ProductWorker &worker, Buffer &buffer, vector<Part> &parts, int num_of_iteration)
+{
+    // each worker to finish 5 iterations
+    // int num_of_iteration = 5;
+
+    while (num_of_iteration-- > 0)
+    {
+        worker.productWorkerWorkflow(buffer, parts);
+    }
+
+    return;
+}
+
 int main()
 {
     srand(time(NULL));
@@ -218,12 +218,13 @@ int main()
         product_workers.push_back(ProductWorker(i, 600));
 
     // push workers into their workflow
+    int num_of_iteration = 5;
     vector<thread> part_worker_threads;
     vector<thread> product_worker_threads;
     for (int i = 0; i < part_workers.size(); i++)
-        part_worker_threads.emplace_back(&PartWorker::partWorkerWorkflow, &part_workers[i], ref(buffer), ref(parts));
+        part_worker_threads.emplace_back(PartWorkerScheduler, ref(part_workers[i]), ref(buffer), ref(parts), num_of_iteration);
     for (int i = 0; i < product_workers.size(); i++)
-        product_worker_threads.emplace_back(&ProductWorker::productWorkerWorkflow, &product_workers[i], ref(buffer), ref(parts));
+        product_worker_threads.emplace_back(ProductWorkerScheduler, ref(product_workers[i]), ref(buffer), ref(parts), num_of_iteration);
     for (thread &i : part_worker_threads)
         i.join();
     for (thread &i : product_worker_threads)
@@ -239,7 +240,70 @@ int main()
 void PartWorker::partWorkerWorkflow(Buffer &buffer, const vector<Part> &parts)
 {
     unique_lock<mutex> UG1(m1);
-    // sleep
+
+    // produce parts we need but not in local state
+    int sleep_time = 0;
+    for (int i = 0; i < load_order.size(); i++)
+        sleep_time += (load_order[i] - local_state[i]) * parts[i].producting_time_comsumption;
+    if (sleep_time != 0)
+        this_thread::sleep_for(microseconds(sleep_time));
+    sleep_time = 0;
+
+    // now we have all the parts we need ready in local state
+    int num_of_part_to_drop = 0;
+    local_state = load_order;
+    for (int part_num : local_state)
+        num_of_part_to_drop += part_num;
+
+    // try to put into buffer - need a lock here for buffer
+    int wait_time = 0;
+    while (true)
+    {
+        // lock to preventing buffer be used by partworker and productworker at the same time
+        if (buffer_lock.try_lock())
+        {
+            // put parts into buffer
+            for (int i = 0; i < local_state.size(); i++)
+            {
+                int num_of_dropped_parts = (buffer.buffer_capacity[i] - buffer.buffer_state[i]) > local_state[i] ? local_state[i] : (buffer.buffer_capacity[i] - buffer.buffer_state[i]);
+
+                buffer.buffer_state[i] += num_of_dropped_parts;
+                local_state[i] -= num_of_dropped_parts;
+
+                sleep_time += num_of_dropped_parts * parts[i].moving_time_consumption;
+            }
+
+            // no access to buffer from the code below
+            buffer_lock.unlock();
+
+            // time calcuation and sleep - no sleep no print
+            wait_time += sleep_time;
+            if (sleep_time != 0)
+                this_thread::sleep_for(microseconds(sleep_time));
+            sleep_time = 0;
+
+            // check wether all the types have been dropped
+            if (num_of_part_to_drop == 0 || wait_time > max_wait_time)
+                break;
+
+            // if can not do any more drop and there are still parts haven't be dropped, wait for notify from productworkers
+            cv1.wait(UG1);
+        }
+    }
+
+    // we need to put the parts left in local state back home
+    sleep_time = 0;
+    if (num_of_part_to_drop != 0)
+    {
+        for (int i = 0; i < local_state.size(); i++)
+            sleep_time += local_state[i] * parts[i].moving_time_consumption;
+
+        if (sleep_time != 0)
+            this_thread::sleep_for(microseconds(sleep_time));
+    }
+
+    // notify productWorkers the buffer have been refilled
+    cv2.notify_all();
 
     return;
 }
